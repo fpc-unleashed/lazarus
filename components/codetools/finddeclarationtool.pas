fpc-unleashed/lazarus

commit 631a133997cabe9e06e4fafd7b7f9d33b7404155

Add inline-var support in begin..end blocks

components/codetools/finddeclarationtool.pas
@@ -5375,11 +5375,13 @@ var
         ctnClassClassVar,
         ctnRecordVariant,
         ctnProcedureHead, ctnParameterList,
-        ctnClassInheritance,ctnHelperFor:
+        ctnClassInheritance,ctnHelperFor,
+        ctnBeginBlock:
           // these codetreenodes build a parent-child-relationship, but
           // for pascal it is only a range, hence after searching in the
           // children of the last node, search must continue in the children
           // of the prior node
+          // Note: ctnBeginBlock is transparent to support inline var declarations
           ;
 
         ctnClass, ctnClassInterface, ctnDispinterface, ctnObject,
@@ -5555,10 +5557,12 @@ begin
           ctnRecordType, ctnRecordVariant,
           ctnClassHelper, ctnRecordHelper, ctnTypeHelper,
           ctnEnumerationType,
-          ctnParameterList:
+          ctnParameterList,
+          ctnBeginBlock:
             // these nodes build a parent-child relationship. But in pascal
             // they just define a range and not a context.
             // -> search in all children
+            // Note: ctnBeginBlock is included to support inline var declarations
             MoveContextNodeToChildren;
 
           ctnTypeDefinition, ctnVarDefinition, ctnConstDefinition,

components/codetools/keywordfunclists.pas
@@ -1544,7 +1544,8 @@ begin
     Add('THREADVAR',{$ifdef FPC}@{$endif}AllwaysTrue);
     Add('TYPE',{$ifdef FPC}@{$endif}AllwaysTrue);
     Add('UNIT',{$ifdef FPC}@{$endif}AllwaysTrue);
-    Add('VAR',{$ifdef FPC}@{$endif}AllwaysTrue);
+    // Note: VAR is intentionally NOT in this list - it is allowed inside
+    // begin..end blocks as an inline variable declaration
   end;
   
   UnexpectedKeyWordInAsmBlock:=TKeyWordFunctionList.Create('UnexpectedKeyWordInAsmBlock');

components/codetools/pascalparsertool.pas
@@ -256,6 +256,7 @@ type
         CreateNodes: boolean): boolean;
     function ReadWithStatement(ExceptionOnError, CreateNodes: boolean): boolean;
     function ReadOnStatement(ExceptionOnError, CreateNodes: boolean): boolean;
+    procedure ReadInlineVarDeclaration(CreateNodes: boolean);
     procedure ReadVariableType;
     function ReadHintModifiers(AllowSemicolonSep: boolean): boolean;
     function ReadTilTypeOfProperty(PropertyNode: TCodeTreeNode): boolean;
@@ -3085,6 +3086,9 @@ begin
       end;
     end else if CreateNodes and UpAtomIs('WITH') then begin
       ReadWithStatement(true,CreateNodes);
+    end else if UpAtomIs('VAR')
+    and (BlockType in [ebtBegin,ebtTry,ebtRepeat]) then begin
+      ReadInlineVarDeclaration(CreateNodes);
     end else if UpAtomIs('ON') and (BlockType=ebtTry)
     and (TryType=ttExcept) then begin
       ReadOnStatement(true,CreateNodes);
@@ -3538,6 +3542,63 @@ begin
   if NeedUndo then
     UndoReadNextAtom;
   Result:=true;
+end;
+
+procedure TPascalParserTool.ReadInlineVarDeclaration(CreateNodes: boolean);
+{ Reads an inline variable declaration inside a begin..end block.
+  Cursor must be on the 'var' keyword.
+  Examples:
+    var s1: string := 'test';
+    var s2 := 'test';
+    var t: SomeRecord;
+  Creates ctnVarSection > ctnVarDefinition nodes when CreateNodes=true.
+}
+var
+  BracketDepth: integer;
+begin
+  if CreateNodes then begin
+    CreateChildNode;
+    CurNode.Desc := ctnVarSection;
+  end;
+  ReadNextAtom; // read identifier name
+  if AtomIsIdentifier then begin
+    if CreateNodes then begin
+      CreateChildNode;
+      CurNode.Desc := ctnVarDefinition;
+    end;
+    // Skip the rest of the declaration until ';'
+    // Track bracket depth to handle nested expressions like func(a, b)
+    BracketDepth := 0;
+    repeat
+      ReadNextAtom;
+      if CurPos.StartPos > SrcLen then break;
+      case CurPos.Flag of
+        cafRoundBracketOpen, cafEdgedBracketOpen:
+          inc(BracketDepth);
+        cafRoundBracketClose, cafEdgedBracketClose:
+          dec(BracketDepth);
+        cafSemicolon:
+          if BracketDepth = 0 then break;
+      end;
+      if (BracketDepth = 0) and (CurPos.Flag = cafWord) then begin
+        if UpAtomIs('END') or UpAtomIs('BEGIN') or UpAtomIs('VAR')
+        or UpAtomIs('UNTIL') or UpAtomIs('FINALLY') or UpAtomIs('EXCEPT')
+        or UpAtomIs('ELSE') or UpAtomIs('THEN') then begin
+          UndoReadNextAtom;
+          break;
+        end;
+      end;
+    until false;
+    if CreateNodes then begin
+      CurNode.EndPos := CurPos.EndPos;
+      EndChildNode; // close ctnVarDefinition
+    end;
+  end else
+    UndoReadNextAtom;
+  if CreateNodes then begin
+    CurNode.EndPos := CurPos.EndPos;
+    EndChildNode; // close ctnVarSection
+  end;
 end;
 
 procedure TPascalParserTool.ReadVariableType;

commit 902632852b376e147c359071d581539f5009edf4

CodeTools: fix with-autocomplete in main begin..end block
Marking ctnBeginBlock transparent in 631a1339 broke autocomplete on
incomplete with-expressions in the program's main begin..end block.
The inline-var autocomplete the original commit aimed to enable still
works without this change.

components/codetools/finddeclarationtool.pas
@@ -5377,13 +5377,11 @@ var
         ctnClassClassVar,
         ctnRecordVariant,
         ctnProcedureHead, ctnParameterList,
-        ctnClassInheritance,ctnHelperFor,
-        ctnBeginBlock:
+        ctnClassInheritance,ctnHelperFor:
           // these codetreenodes build a parent-child-relationship, but
           // for pascal it is only a range, hence after searching in the
           // children of the last node, search must continue in the children
           // of the prior node
-          // Note: ctnBeginBlock is transparent to support inline var declarations
           ;
 
         ctnClass, ctnClassInterface, ctnDispinterface, ctnObject,
@@ -5559,12 +5557,10 @@ begin
           ctnRecordType, ctnRecordVariant,
           ctnClassHelper, ctnRecordHelper, ctnTypeHelper,
           ctnEnumerationType,
-          ctnParameterList,
-          ctnBeginBlock:
+          ctnParameterList:
             // these nodes build a parent-child relationship. But in pascal
             // they just define a range and not a context.
             // -> search in all children
-            // Note: ctnBeginBlock is included to support inline var declarations
             MoveContextNodeToChildren;
 
           ctnTypeDefinition, ctnVarDefinition, ctnConstDefinition,